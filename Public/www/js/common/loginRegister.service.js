6ccf158a9333f23daa16f069b3a45c950c6703c8
Agronis <agronis@icloud.com>
2016-01-03 21:43:25 -0500

    Yeah.. 810..?

--- Public/www/js/common/loginRegister.service.js
+++ Public/www/js/common/loginRegister.service.js
@@ -5,7 +5,7 @@
 
     .module('schindig')
     .factory('LoginRegisterService', function($http, $state, $q, $cordovaDevice, $cordovaToast){
-      var ip = 'http://localhost:8080';
+      var ip = 'http://104.236.244.159:8080/';
       var registerUrl = ip + '/user/create';
       var loginUrl = ip + '/user/login';
 
