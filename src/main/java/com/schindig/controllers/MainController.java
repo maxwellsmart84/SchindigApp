cd44ab30f2419ed17e6242aaf35ca041daf3fe7b
Agronis <agronis@icloud.com>
2016-01-03 19:36:49 -0500

    again

--- src/main/java/com/schindig/controllers/MainController.java
+++ src/main/java/com/schindig/controllers/MainController.java
@@ -868,7 +868,7 @@ public class MainController {
 
     @RequestMapping(path = "/venmo/{partyID}/{userID}", method = RequestMethod.GET)
     public void goVenmo(HttpServletResponse response, @PathVariable("userID") Integer userID, HttpServletRequest request, @PathVariable("partyID") Integer partyID) throws IOException {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8100");
         response.sendRedirect(Venmo.getFrontEnd().concat("&state="+partyID+":"+userID));
     }
 
