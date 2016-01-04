84acee112562cf433376d16ec470a0942eabbaf4
Maxwell Krause <maxwell.l.krause@gmail.com>
2016-01-04 15:27:38 -0500

    new styling

--- Public/www/js/common/loginRegister.service.js
+++ Public/www/js/common/loginRegister.service.js
@@ -7,7 +7,7 @@
     .factory('LoginRegisterService', function($http, $state, $q, $cordovaDevice, $cordovaToast){
 
       var ip = "http://localhost:8080";
-      // var ip = "http://104.236.244.159:8100"; 
+      // var ip = "http://104.236.244.159:8100";
       var registerUrl = ip + '/user/create';
       var loginUrl = ip + '/user/login';
 
